f106a7de471c26e317b5351d7022dc84404c2180
Jamil Matheny <jamil.matheny@gmail.com>
2020-10-05 12:10:47 -0400

    written comments

--- HelloJM/HelloWorld.cs
+++ HelloJM/HelloWorld.cs
@@ -5,6 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
+/* 
+Name: Jamil Matheny
+Date: 10/05/2020
+Class: CIS214
+ */
+
+
 // Namespace is the environment in which the entire program will work.
 namespace HelloJM
 {
@@ -12,14 +19,29 @@ namespace HelloJM
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Hello World \n"); // output statement
+			Console.Write("Hello World \n"); // output statement without a break
 			Console.WriteLine("My name is Jamil Matheny"); // output statement with newline
 			Console.Write("Testing 123");
 
-			string dayOfWeek = "Monday";
-			string myName = "Dr. H";
+			//formatting text
+			Console.WriteLine("\n{0}\n{1}\n{2}", "Working with", "text in", "C#...");
 
-			Console.Write($ "Hello World on,  {dayOfWeek}");
+			string dayOfWeek = "Monday";   // 'string' is a primitive datatype // 'dayOfWeek' is the variable which is stored memory that can be changed. 
+			string myName = "Jamil Matheny";
+
+
+			//string interpolation
+			Console.Write($"Hello World on,  {dayOfWeek} oh no another one"); // '$' is formatted text string w/ the variable
+			Console.WriteLine($" with, {myName} !!!"); // use lower camel casing to describe the variables. Every variable has a name, type, size and value.
+
+			//Condition is an expression that can be either true or false.
+
+			/* 
+			 Relational operators  
+				>, < less than or greater than
+			 Equal operators
+				== equal to,  != not equal to
+			 */
 
 
 		}
